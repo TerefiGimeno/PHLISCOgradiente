a85563343817525c3188ad0f407e5d4dd86abb87
TerefiGimeno <tegimeno@gmail.com>
2025-12-11 16:31:19 +0100

    still cleaning data

--- gradienteRead/cleanIsotopeData.R
+++ gradienteRead/cleanIsotopeData.R
@@ -1,6 +1,15 @@
 library(tidyr)
 library(dplyr)
 
+trees <- read.csv("gradienteData/isotopes_gradiente_2023/Tabla_S2025-3401_mod.csv") %>%
+  right_join(read.csv("gradienteData/alturas_individuos/dbh_height.csv"),
+             by = c("site", "tree")) %>% 
+  filter(year == 2023) %>% 
+  select(-c(year, perc_C)) %>%
+  rename(d13C_ring23 = d13C_permil) %>% 
+  mutate(campaign = c(rep("winter24", times = nrow(trees))))
+  
+    
 d13Cphloem <-
   read.csv("gradienteData/isotopes_gradiente_2023/table01.csv") %>% 
   filter(stringr::str_detect(id, "_")) %>% 
