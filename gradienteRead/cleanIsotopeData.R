5c72a729b7b13cd7d50c5365a5de96179c0d19ea
TerefiGimeno <tegimeno@gmail.com>
2026-02-13 16:16:41 +0100

    correct things

--- gradienteRead/cleanIsotopeData.R
+++ gradienteRead/cleanIsotopeData.R
@@ -3,30 +3,37 @@ library(tidyverse)
 # selection of data from the various campaigns and canopy positions is based on
 # the results of the statistical analyses detailed in "quick_stats_plots"
 
+d13CtreeRing <- read.csv("gradienteData/isotopes_gradiente_2023/Tabla_S2025-3401_mod.csv") %>% 
+  full_join(read.csv("gradienteData/alturas_individuos/dbh_height.csv"),
+             by = c("site", "tree")) %>% 
+  filter(year == 2023) %>% 
+  select(-c(year, perc_C)) %>%
+  rename(d13C_ring23 = d13C_permil) %>% 
+  relocate(d13C_ring23, .after = h_m)
+
+d13CbasePh <- read.csv("gradienteData/isotopes_gradiente_2023/isotopes_base_phloem.csv") %>% 
+  #full_join(read.csv("gradienteData/alturas_individuos/dbh_height.csv"),
+   #         by = c("site", "tree")) %>%
+  filter(sampling_date <= 20230701 | sampling_date >= 20230827) %>%
+  mutate(campaign = ifelse(sampling_date <= 20230701, "spring23", "summer23")) %>% 
+  select(-c(d15N_base_phloem)) %>% 
+  relocate(dbh_cm, .after = tree) %>% 
+  relocate(h_m, .after = dbh_cm)
+
 d13Cleaf <- read.csv("gradienteData/isotopes_gradiente_2023/isotopes_leaf.csv") %>% 
   mutate(ratio_CN_leaf = C_perc_leaf/N_perc_leaf) %>% 
   filter(canopy_position == "shade_low") %>% 
   filter(sampling_date <= 20230731 | sampling_date >= 20230827) %>% 
   select(-c(weight_mg, d15N_leaf, canopy_position, canopy_position2, sampling_date))
 
-d13CbasePh <- read.csv("gradienteData/isotopes_gradiente_2023/isotopes_base_phloem.csv") %>% 
-  filter(sampling_date <= 20230701 | sampling_date >= 20230827) %>%
-  mutate(campaign = ifelse(sampling_date <= 20230701, "spring23", "summer23")) %>% 
-  select(-c(d15N_base_phloem))
-
-d13CtreeRing <- read.csv("gradienteData/isotopes_gradiente_2023/Tabla_S2025-3401_mod.csv") %>% 
-  filter(year == 2023) %>% 
-  select(-c(year, perc_C)) %>%
-  rename(d13C_ring23 = d13C_permil) %>% 
-  
 d13CstemPh <- read.csv("gradienteData/isotopes_gradiente_2023/isotopes_stem_phloem.csv") %>%
   filter(sampling_date <= 20230701 | sampling_date >= 20230827) %>%
   filter(canopy_position == "shade_low") %>%
   mutate(campaign = ifelse(sampling_date <= 20230701, "spring23", "summer23")) %>% 
   select(-c(d15N_stem_phloem, canopy_position, canopy_position2, sampling_date))
 
-trees <- read.csv("gradienteData/isotopes_gradiente_2023/Tabla_S2025-3401_mod.csv") %>%
-  right_join(read.csv("gradienteData/alturas_individuos/dbh_height.csv"),
+trees <-  %>%
+  right_join(read.csv(),
              by = c("site", "tree")) %>% 
   
 
--- gradienteRead/quick_stats_plots.R
+++ gradienteRead/quick_stats_plots.R
@@ -11,7 +11,7 @@ d13Cleaf <- read.csv("gradienteData/isotopes_gradiente_2023/isotopes_leaf.csv") 
   mutate(ratio_CN_leaf = C_perc_leaf/N_perc_leaf) %>% 
   
 d13CbasePh <- read.csv("gradienteData/isotopes_gradiente_2023/isotopes_base_phloem.csv") %>% 
-  select(-c(d15N_base_phloem)) %>% 
+  select(-c(d15N_base_phloem))
   
 d13CtreeRing <- read.csv("gradienteData/isotopes_gradiente_2023/Tabla_S2025-3401_mod.csv") %>% 
   filter(year == 2023) %>% 
