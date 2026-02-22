6cbddf1261264e382d7ed93c00436ce53ca35820
TerefiGimeno <tegimeno@gmail.com>
2026-02-22 12:56:22 +0100

    organize files analyses

--- gradienteRead/correlations.R
+++ gradienteRead/correlations.R
@@ -1,24 +1,18 @@
 library(tidyverse)
 
-# selection of data from the various campaigns and canopy positions is based on
-# the results of the statistical analyses detailed in "quick_stats_plots"
+####1. build the database####
 
 d13CtreeRing <- read.csv("gradienteData/isotopes_gradiente_2023/Tabla_S2025-3401_mod.csv") %>% 
   full_join(read.csv("gradienteData/alturas_individuos/dbh_height.csv"),
-             by = c("site", "tree")) %>% 
+            by = c("site", "tree")) %>% 
   filter(year == 2023) %>% 
   select(-c(year, perc_C)) %>%
-  rename(d13C_ring23 = d13C_permil) %>% 
-  relocate(d13C_ring23, .after = h_m)
+  rename(d13C_ring23 = d13C_permil)
 
 d13CbasePh <- read.csv("gradienteData/isotopes_gradiente_2023/isotopes_base_phloem.csv") %>% 
-  #full_join(read.csv("gradienteData/alturas_individuos/dbh_height.csv"),
-   #         by = c("site", "tree")) %>%
   filter(sampling_date <= 20230701 | sampling_date >= 20230827) %>%
   mutate(campaign = ifelse(sampling_date <= 20230701, "spring23", "summer23")) %>% 
-  select(-c(d15N_base_phloem)) %>% 
-  relocate(dbh_cm, .after = tree) %>% 
-  relocate(h_m, .after = dbh_cm)
+  select(-c(d15N_base_phloem, sampling_date)) 
 
 d13Cleaf <- read.csv("gradienteData/isotopes_gradiente_2023/isotopes_leaf.csv") %>% 
   mutate(ratio_CN_leaf = C_perc_leaf/N_perc_leaf) %>% 
@@ -32,3 +26,10 @@ d13CstemPh <- read.csv("gradienteData/isotopes_gradiente_2023/isotopes_stem_phlo
   mutate(campaign = ifelse(sampling_date <= 20230701, "spring23", "summer23")) %>% 
   select(-c(d15N_stem_phloem, canopy_position, canopy_position2, sampling_date))
 
+wp <- read.csv("gradienteData/wp_gradiente_2023/lwp_gradiente_2023.csv") %>% 
+  mutate(site = factor(site, levels = c("ART", "BER", "ITU", "MSA", "DIU"))) %>% 
+  mutate(wp_md = (wp_midday_1 + wp_midday_2)/2)
+
+gradient <- d13CtreeRing %>% 
+  full_join(wp, by = c("site", "tree")) %>% 
+  relocate(d13C_ring23, .after = wp_md)
--- gradienteRead/wp.R
+++ gradienteRead/wp.R
@@ -1,8 +1,34 @@
 library(tidyverse)
 
-wp <- 
+wp <- read.csv("gradienteData/wp_gradiente_2023/lwp_gradiente_2023.csv") %>% 
+  mutate(site = factor(site, levels = c("ART", "BER", "ITU", "MSA", "DIU"))) %>% 
+  mutate(wp_md = (wp_midday_1 + wp_midday_2)/2)
 
+plot(wp$wp_md, pch = 19, col ="blue", ylim = c(-3.5, -0.2))
+points(wp$wp_midday_1, pch = 19, col = "red")
+points(wp$wp_midday_2, pch = 19, col = "green")
 
-d13CtreeRing <- read.csv("gradienteData/isotopes_gradiente_2023/Tabla_S2025-3401_mod.csv") %>% 
-  full_join(read.csv("gradienteData/alturas_individuos/dbh_height.csv"),
-            by = c("site", "tree"))
+# some differences between replicates within leaves, but within range.
+# We do not discard any values.
+
+hist(wp$wp_md)
+summary(lm(wp_md ~ site * campaign, data = wp))
+anova(lm(wp_md ~ site * campaign, data = wp))
+TukeyHSD(aov(wp_md ~ site, data = wp))
+TukeyHSD(aov(wp_md ~ site *campaign, data = wp))
+
+ggplot(wp, aes(x = site, y = wp_md)) +
+  geom_boxplot(position = position_dodge(width = 0.8))
+
+ggplot(wp, aes(x = site, y = wp_md, fill = campaign)) +
+  geom_boxplot(position = position_dodge(width = 0.8)) +
+  labs(
+    x = "",
+    y = expression(Psi[md]~"(MPa)"),
+    fill = "Campaign"
+  ) +
+  theme_minimal()
+
+# differences between seasons: more negative wp_md in summer
+# differences among sites: (ART = BER)ab = (DIU)b < ITUab
+
