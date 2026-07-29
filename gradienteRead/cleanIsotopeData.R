00c9ee2ac845a6733279cdf729adcf2bc91868ec
TerefiGimeno <tegimeno@gmail.com>
2026-07-29 16:35:35 +0200

    final anovas

--- gradienteRead/cleanIsotopeData.R
+++ gradienteRead/cleanIsotopeData.R
@@ -2,6 +2,7 @@ library(emmeans)
 library(multcomp)
 library(multcompView)
 library(tidyverse)
+library(ggsignif)
 
 ####1. Prepare the data####
 
@@ -40,10 +41,12 @@ d13C_gradiente <- full_join(d13CstemPh, d13CbasePh, by = c("site", "tree", "camp
   full_join(d13CtreeRing, by = c("site", "tree")) |> 
   relocate(c(dbh_cm, h_m, d13C_ring23), .after = campaign) |> 
   relocate(sampling_date, .after = campaign) |> 
-  mutate(site = factor(site, levels = c("ART", "BER", "ITU", "MSA", "DIU", "HMO")))
+  mutate(site = factor(site, levels = c("ART", "BER", "ITU", "MSA", "DIU", "HMO"))) |> 
+  mutate(campaign = ifelse(campaign == "spring23", "Spring", "Summer"))
 
 ####2. Analyses####
-#####2.1. d15N#####
+#####2.1. Leaf stoichiometry#####
+######2.1.1. d15N######
 hist(d13C_gradiente$d15N_leaf)
 modeld15N <- lm(d15N_leaf ~ site * campaign, data = d13C_gradiente)
 plot(modeld15N)
@@ -63,7 +66,7 @@ ggplot(d13C_gradiente, aes(x = site, y = d15N_leaf, fill = campaign)) +
     fill = "Campaign"
   ) +
   theme_minimal()
-#####2.1. Nitrogen#####
+######2.1.2. Nitrogen######
 hist(d13C_gradiente$N_perc_leaf)
 modelN <- lm(N_perc_leaf ~ site * campaign, data = d13C_gradiente)
 plot(modelN)
@@ -84,7 +87,7 @@ ggplot(d13C_gradiente, aes(x = site, y = N_perc_leaf, fill = campaign)) +
   ) +
   theme_minimal()
 
-#####2.1. C:N#####
+######2.1.3. C:N######
 hist(d13C_gradiente$ratio_CN_leaf)
 modelratio <- lm(ratio_CN_leaf ~ site * campaign, data = d13C_gradiente)
 plot(modelratio)
@@ -104,3 +107,158 @@ ggplot(d13C_gradiente, aes(x = site, y = ratio_CN_leaf, fill = campaign)) +
     fill = "Campaign"
   ) +
   theme_minimal()
+
+#####2.2. d13C#####
+######2.2.1 Leaf d13C######
+hist(d13C_gradiente$d13C_leaf)
+modeld13C_leaf <- lm(d13C_leaf ~ site * campaign, data = d13C_gradiente)
+plot(modeld13C_leaf)
+summary(modeld13C_leaf)
+anova(modeld13C_leaf)
+modeld13Cleaf_means <- emmeans(modeld13C_leaf, ~ site)
+model_means_cld <- cld(modeld13Cleaf_means, adjust = "sidak",
+                       Letters = c("a", "b", "c", "d", "e", "f", "g"),
+                       alpha = 0.05, sort = FALSE)
+ggplot(d13C_gradiente, aes(x = site, y = d13C_leaf, fill = campaign)) +
+  geom_boxplot(
+    position = position_dodge2(width = 0.8, preserve = "single")) +
+  geom_signif(y_position=c(-27.1, -27.1, -26.5), xmin=c(0.65, 3.5, 0.65), 
+              xmax=c(3.45, 5.5, 5.5), annotation=c("ns", "ns", "*"),
+              tip_length=0.06, vjust = -0.2, textsize = 6, color="grey40") +
+  scale_fill_manual(values=c("#EF476F", "#FFD166")) +
+  ylim(-35, -26)+
+  labs(
+    x = "",
+    y = expression("Bulk " * delta^13*C[leaf]~"(\u2030)"),
+    fill = "Campaign"
+  ) +
+  theme(
+    panel.background = element_blank(),
+    plot.background  = element_blank(),
+    panel.border = element_rect(color = "black",
+                                fill = NA,
+                                linewidth = .5),
+    axis.line = element_blank(),
+    axis.title.y = element_text(size = 15),
+    axis.text.x  = element_text(size = 13),
+    axis.text.y  = element_text(size = 12),
+    legend.title = element_blank(),
+    legend.text = element_text(size = 11),
+    legend.position = c(0.98, 0.05),
+    legend.justification = c("right", "bottom"),
+    legend.background = element_blank(),
+    legend.key = element_blank(),
+    panel.grid = element_blank()
+  )
+
+######2.2.2 Branch phloem d13C######
+hist(d13C_gradiente$d13C_branch_ph)
+modeld13C_branch_ph <- lm(d13C_branch_ph ~ site * campaign, data = d13C_gradiente)
+plot(modeld13C_branch_ph)
+summary(modeld13C_branch_ph)
+anova(modeld13C_branch_ph)
+modeld13C_branch_ph_means <- emmeans(modeld13C_branch_ph, ~ site * campaign)
+model_means_cld <- cld(modeld13C_branch_ph_means, adjust = "sidak",
+                       Letters = c("a", "b", "c", "d", "e", "f", "g"),
+                       alpha = 0.05, sort = FALSE)
+ggplot(d13C_gradiente, aes(x = site, y = d13C_branch_ph, fill = campaign)) +
+  geom_boxplot(
+    position = position_dodge2(width = 0.8, preserve = "single")) +
+  scale_fill_manual(values=c("#EF476F", "#FFD166")) +
+  ylim(-35, -26)+
+  labs(
+    x = "",
+    y = expression("Phloem " * delta^13*C[branch]~"(\u2030)"),
+    fill = "Campaign"
+  ) +
+  theme(
+    panel.background = element_blank(),
+    plot.background  = element_blank(),
+    panel.border = element_rect(color = "black",
+                                fill = NA,
+                                linewidth = .5),
+    axis.line = element_blank(),
+    axis.title.y = element_text(size = 15),
+    axis.text.x  = element_text(size = 13),
+    axis.text.y  = element_text(size = 12),
+    legend.title = element_blank(),
+    legend.text = element_text(size = 11),
+    legend.position = c(0.98, 0.05),
+    legend.justification = c("right", "bottom"),
+    legend.background = element_blank(),
+    legend.key = element_blank(),
+    panel.grid = element_blank()
+  )
+
+######2.2.3 Stem phloem d13C######
+hist(d13C_gradiente$d13C_stem_ph)
+modeld13C_stem_ph <- lm(d13C_stem_ph ~ site * campaign, data = d13C_gradiente)
+plot(modeld13C_stem_ph)
+summary(modeld13C_stem_ph)
+anova(modeld13C_stem_ph)
+modeld13C_stem_ph_means <- emmeans(modeld13C_stem_ph, ~ site * campaign)
+model_means_cld <- cld(modeld13C_stem_ph_means, adjust = "sidak",
+                       Letters = c("a", "b", "c", "d", "e", "f", "g"),
+                       alpha = 0.05, sort = FALSE)
+ggplot(d13C_gradiente, aes(x = site, y = d13C_stem_ph, fill = campaign)) +
+  geom_boxplot(
+    position = position_dodge2(width = 0.8, preserve = "single")) +
+  scale_fill_manual(values=c("#EF476F", "#FFD166")) +
+  ylim(-35, -24)+
+  labs(
+    x = "",
+    y = expression("Phloem " * delta^13*C[stem]~"(\u2030)"),
+    fill = "Campaign"
+  ) +
+  theme(
+    panel.background = element_blank(),
+    plot.background  = element_blank(),
+    panel.border = element_rect(color = "black",
+                                fill = NA,
+                                linewidth = .5),
+    axis.line = element_blank(),
+    axis.title.y = element_text(size = 15),
+    axis.text.x  = element_text(size = 13),
+    axis.text.y  = element_text(size = 12),
+    legend.title = element_blank(),
+    legend.text = element_text(size = 11),
+    legend.position = c(0.98, 0.05),
+    legend.justification = c("right", "bottom"),
+    legend.background = element_blank(),
+    legend.key = element_blank(),
+    panel.grid = element_blank()
+  )
+
+######2.2.4 Ring cellulose d13C######
+hist(d13C_gradiente$d13C_ring23)
+modeld13C_ring <- lm(d13C_ring23 ~ site, data = d13C_gradiente)
+plot(modeld13C_ring)
+summary(modeld13C_ring)
+anova(modeld13C_ring)
+modeld13C_ring_means <- emmeans(modeld13C_ring, ~ site)
+model_means_cld <- cld(modeld13C_ring_means, adjust = "sidak",
+                       Letters = c("a", "b", "c", "d", "e", "f", "g"),
+                       alpha = 0.05, sort = FALSE)
+ggplot(d13C_gradiente, aes(x = site, y = d13C_ring23, fill = "#06D6A0")) +
+  geom_boxplot(
+    position = position_dodge2(width = 0.8, preserve = "single")) +
+  ylim(-35, -24.3)+
+  labs(
+    x = "",
+    y = expression("Cellulose " * delta^13*C[ring]~"(\u2030)"),
+    ) +
+  theme(
+    panel.background = element_blank(),
+    plot.background  = element_blank(),
+    panel.border = element_rect(color = "black",
+                                fill = NA,
+                                linewidth = .5),
+    axis.line = element_blank(),
+    axis.title.y = element_text(size = 15),
+    axis.text.x  = element_text(size = 13),
+    axis.text.y  = element_text(size = 12),
+    )
+
+
+
+
